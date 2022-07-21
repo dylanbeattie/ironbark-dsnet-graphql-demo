fd7395bb28aa07c69e64ade03aa54d591649e6b9
Dylan Beattie <dylan@dylanbeattie.net>
2022-07-21 06:41:39 +0100

    Pushing changes from GraphQL demo session

---
+++ dotnet/Autobarn.Website/GraphQL/Queries/VehicleQuery.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Autobarn.Data;
+using Autobarn.Data.Entities;
+using Autobarn.Website.GraphQL.GraphTypes;
+using GraphQL;
+using GraphQL.Types;
+
+namespace Autobarn.Website.GraphQL.Queries {
+    public class VehicleQuery : ObjectGraphType {
+        private readonly IAutobarnDatabase db;
+
+        public VehicleQuery(IAutobarnDatabase db) {
+            this.db = db;
+
+            Field<ListGraphType<VehicleGraphType>>("Vehicles", "Query to retrieve all Vehicles",
+                resolve: GetAllVehicles);
+
+            Field<VehicleGraphType>("Vehicle", "Query to retrieve a specific Vehicle",
+                new QueryArguments(MakeNonNullStringArgument("registration", "The registration (licence plate) of the Vehicle")),
+                resolve: GetVehicle);
+        }
+
+        private Vehicle GetVehicle(IResolveFieldContext<object> context) {
+            var registration = context.GetArgument<string>("registration");
+            return db.FindVehicle(registration);
+        }
+
+        private IEnumerable<Vehicle> GetAllVehicles(IResolveFieldContext<object> arg) {
+            return db.ListVehicles();
+        }
+
+        private QueryArgument MakeNonNullStringArgument(string name, string description) {
+            return new QueryArgument<NonNullGraphType<StringGraphType>> {
+                Name = name, Description = description
+            };
+        }
+    }
+}
